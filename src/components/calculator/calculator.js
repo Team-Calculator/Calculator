fa5ef33ed00a7794ab97917ad86b0bf83e9e49cf
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-02-05 11:23:39 +0100

    html structure

---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,17 @@
+import { createElement } from "../../components/calculator/utils";
+
+<input ="upper display"></input>
+
+<div class"Number Field">
+<button class="Number">1</button>
+</div>
+
+<div class="Result"> = <button class"Result"></button></div>
+
+<div class="Operations">
+<button class"Plus">+</button>
+<button class"Plus">-</button>
+<button class"Plus">x</button>
+<button class"Plus">÷</button>
+</div>
+ 
